695179b2ed41da8315019ad661897eacbd4dfaa0
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2024-09-04 22:07:55 +0300

    fix: app refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,56 @@
 import http from 'http';
-import {createHash} from 'crypto';
-// установить rimraf для очистки папки проекта перед сборкой
 
-import mysql from 'mysql2/promise'; // написать свой драйвер
+import {LoggerService} from './logger.service.js';
+import {BusinessRUService} from './business-ru.service.js';
 
-type BruToken = { // добавить в настройки линтера обработку TS
-  token: string;
-  request_count: string;
-  app_psw: string;
-};
+import {Source} from './types/source.enum.js';
+import {Good} from './types/good.type.js';
+import {GoodsRepository} from './goods.repository.js';
 
 const host = process.env.HOST;
 const port = Number(process.env.PORT);
 
-const bruAccount = process.env.BRU_ACCOUNT;
-const appId = process.env.APP_ID;
-const bruSecret = process.env.SECRET;
+const logger = new LoggerService();
+const businessRUService = new BusinessRUService();
+const goodsRepository = new GoodsRepository();
 
 const server = http.createServer(async (req, res) => {
-  // вынести в отдельную функцию md5
-  const md5QueryString = createHash('md5').update(`${bruSecret}app_id=${appId}`).digest('hex');
-  const bruTokenFetch = await fetch(`https://${bruAccount}.business.ru/api/rest/repair.json?app_id=${appId}&app_psw=${md5QueryString}`);
-  const bruTokenResponse = await bruTokenFetch.json() as BruToken;
-  const bruToken = bruTokenResponse.token;
-
-  // в отдельный класс
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
-
   try {
-    const md5GoodsCountQueryString = createHash('md5').update(`${bruToken}${bruSecret}app_id=${appId}&count_only=1`).digest('hex');
-    const goodsCountFetch = await fetch(`https://a81455.business.ru/api/rest/goods.json?app_id=485783&count_only=1&app_psw=${md5GoodsCountQueryString}`);
-    const goodsCountResponse = await goodsCountFetch.json() as {result: {count: number}};
-    const goodsCount = goodsCountResponse.result.count;
+    const goodsCount = await businessRUService.getItemsCount(Source.Goods);
+    const pagesCount = goodsCount ? Math.ceil(goodsCount / 250)+1 : 0;
 
-    const pagesCount = Math.ceil(goodsCount / 250);
+    if (pagesCount) {
+      await goodsRepository.createConnection();
 
-    for(let page = 1; page <= pagesCount; page++) {
-      const md5GoodsQueryString = createHash('md5').update(`${bruToken}${bruSecret}app_id=${appId}&limit=250&page=${page}`).digest('hex');
-      const goodsFetch = await fetch(`https://${bruAccount}.business.ru/api/rest/goods.json?app_id=${appId}&limit=250&page=${page}&app_psw=${md5GoodsQueryString}`);
-      // перенести в тип
-      const goodsResponse = await goodsFetch.json() as {result: {
-        id: string;
-        name: string;
-        full_name: string;
-        group_id: string;
-        measure_id: string;
-      }[]};
-      const goods = goodsResponse.result;
+      for(let page = 1; page <= pagesCount; page++) {
+        const goods = await businessRUService.getItems<Good>(Source.Goods, page);
 
-      goods.forEach(async (good) => {
-        // Проверка наличия текущего элемента в БД по good.id
-        const [results, fields] = await connection.query(
-          `SELECT count(id) FROM goods WHERE EXISTS (SELECT * FROM goods WHERE bru_id=${good.id})`
-        );
-        const result = results as {'count(id)': number}[];
-        const goodAlreadyExists = result[0]['count(id)'] === 0;
-  
-        if (goodAlreadyExists) {
-          // Имя менять нельзя! Сделать запрос с плейсхолдерами.
-          const goodName = good.name.replace(/\"/g, '\'');
-          const goodFullName = good.full_name.replace(/\"/g, '\'');
+        if (goods && goods.length) {
+          for(let i = 0; i < goods.length; i++) {
+            const goodFromDB = await goodsRepository.find(goods[i].id);
+            const goodExists = goodFromDB && (goodFromDB as any).length;
 
-          await connection.query(
-            `INSERT INTO goods VALUES (NULL, "${goodName}", "${goodFullName}", ${good.id}, ${good.group_id}, ${good.measure_id})`
-          );
+            if (!goodExists) {
+              logger.info(JSON.stringify(goods[i]));
+              await goodsRepository.add(goods[i]);
+            } else {
+              logger.warn('[App]: Insert aborted! Such item already exists in DB!');
+            }
+          }
         }
-      });
-    }
+      }
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    // res.end(JSON.stringify(results));
-    res.end(JSON.stringify([]));
-  } catch (err) {
-    console.log(err);
-  } finally {
-    connection.end();
+      goodsRepository.destroyConnection();
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({result: {goodsCount}}));
+    }
+  } catch(error) {
+    logger.error(`[App]: ${String(error)}`);
   }
 });
 
 server.listen(port, host, () => {
-  console.log(`Server started on ${host}:${port}`);
+  logger.info(`[App]: Server started on ${host}:${port}`);
 });
