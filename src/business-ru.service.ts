bff1559f1d1b9031e610995db9d29a1b9534ccf8
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2024-09-04 22:06:13 +0300

    feat: adds getItems method into BusinessRUService

--- src/business-ru.service.ts
+++ src/business-ru.service.ts
@@ -6,8 +6,10 @@ import {BruToken} from './types/bru-token.type.js';
 import {Query} from './types/query.type.js';
 import {Source} from './types/source.enum.js';
 import {ItemsCount} from './types/items-count.type.js';
+import {Result} from './types/result.type.js';
 
 export class BusinessRUService {
+  private token: string | null = null;
 
   constructor(
     private readonly bruAccount = process.env.BRU_ACCOUNT,
@@ -57,13 +59,16 @@ export class BusinessRUService {
   };
 
   public getItemsCount = async (source: Source) => {
-    const token = await this.getToken();
+    if (!this.token) {
+      const token = await this.getToken();
+      this.token = token;
+    }
 
-    if (this.bruSecret && token) {
+    if (this.bruSecret && this.token) {
       this.loggerService.info(`[BusinessRUService]: ${source} count has been requested`);
 
       const queryString = `app_id=${this.appId}&count_only=1`;
-      const appPsw = this.getMD5String([token, this.bruSecret, queryString]);
+      const appPsw = this.getMD5String([this.token, this.bruSecret, queryString]);
 
       const itemsCountResponse = await this.query({
         source,
@@ -77,6 +82,35 @@ export class BusinessRUService {
       return itemsCount;
     } else {
       this.loggerService.error(`[BusinessRUService]: ${source} count has not been received`);
+
+      return null;
+    }
+  };
+
+  public getItems = async <T>(source: Source, page = 1, limit = 250) => {
+    if (!this.token) {
+      const token = await this.getToken();
+      this.token = token;
+    }
+
+    if (this.bruSecret && this.token) {
+      // this.loggerService.info(`[BusinessRUService.getItems]: ${source} have been requested`);
+
+      const queryString = `app_id=${this.appId}&limit=${limit}&page=${page}`;
+      const appPsw = this.getMD5String([this.token, this.bruSecret, queryString]);
+
+      const itemsResponse = await this.query({
+        source,
+        queryString,
+        appPsw
+      }) as Result<T>;
+      const items = itemsResponse.result;
+
+      // this.loggerService.info(`[BusinessRUService.getItems]: ${source} have been received`);
+
+      return items;
+    } else {
+      this.loggerService.error(`[BusinessRUService.getItems]: ${source} have not been received`);
 
       return null;
     }
