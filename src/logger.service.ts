9314083792a37d9f0818088fa275ec47ec69fe3b
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2024-09-04 15:35:15 +0300

    feat: adds LoggerService

---
+++ src/logger.service.ts
@@ -0,0 +1,25 @@
+import pino, {Logger} from 'pino';
+
+export class LoggerService {
+  private logger: Logger;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public error(message: string, ...args: unknown[]): void {
+    this.logger.error(message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+}
